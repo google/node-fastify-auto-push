5304d6daf94fdcbe6e2bdb082aa8a986c00b1826
Jinwoo Lee <jinwoo68@gmail.com>
2017-11-07 11:48:44 -0800

    update packages

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,13 +1,13 @@
-import * as autoPush from 'auto-push';
 import * as cookie from 'cookie';
 import * as fastify from 'fastify';
+import * as autoPush from 'h2-auto-push';
 import * as http from 'http';
 import * as http2 from 'http2';
 import fp = require('fastify-plugin');
 import * as send from 'send';
 import * as stream from 'stream';
 
-export {AssetCacheConfig} from 'auto-push';
+export {AssetCacheConfig} from 'h2-auto-push';
 
 export interface AutoPushOptions extends fastify.RegisterOptions {
   root: string;
@@ -43,36 +43,48 @@ function staticServeFn(
   if (prefix[0] !== '/') prefix = '/' + prefix;
   if (prefix[prefix.length - 1] !== '/') prefix += '/';
   app.get(prefix + '*', async (req: Request, res: Response) => {
-    if (!isHttp2Request(req.req)) {
-      throw new Error('auto-push middleware can only be used with http2');
+    const reqPath = req.params['*'] || '/';
+    if (isHttp2Request(req.req)) {
+      const reqStream = req.req.stream;
+      const cookies = cookie.parse(req.req.headers['cookie'] as string || '');
+      const cacheKey = cookies[CACHE_COOKIE_KEY];
+      const newCacheKey =
+          await ap.preprocessRequest(reqPath, reqStream, cacheKey);
+      cookies[CACHE_COOKIE_KEY] = newCacheKey;
+      // TODO(jinwoo): Consider making this persistent across sessions.
+      res.header('set-cookie', cookie.serialize(CACHE_COOKIE_KEY, newCacheKey));
+
+      send(req.req, reqPath, {root})
+          .on('error',
+              (err) => {
+                if (err.code === 'ENOENT') {
+                  ap.recordRequestPath(reqStream.session, reqPath, false);
+                  res.code(404).send();
+                } else {
+                  res.code(500).send(err);
+                }
+              })
+          .on('headers',
+              () => {
+                ap.push(reqStream);
+              })
+          .on('end',
+              () => {
+                ap.recordRequestPath(reqStream.session, reqPath, true);
+              })
+          .pipe(res.res as stream.Writable);
+    } else {
+      send(req.req, reqPath, {root})
+          .on('error',
+              (err) => {
+                if (err.code === 'ENOENT') {
+                  res.code(404).send();
+                } else {
+                  res.code(500).send(err);
+                }
+              })
+          .pipe(res.res as stream.Writable);
     }
-
-    const reqPath = req.req.headers[':path'] as string;
-    const reqStream = req.req.stream;
-    const cookies = cookie.parse(req.req.headers['cookie'] as string || '');
-    const cacheKey = cookies[CACHE_COOKIE_KEY];
-    const newCacheKey =
-        await ap.preprocessRequest(reqPath, reqStream, cacheKey);
-    cookies[CACHE_COOKIE_KEY] = newCacheKey;
-    // TODO(jinwoo): Consider making this persistent across sessions.
-    res.header('set-cookie', cookie.serialize(CACHE_COOKIE_KEY, newCacheKey));
-
-    send(req.req, reqPath, {root})
-        .on('error',
-            (err) => {
-              if (err.code === 'ENOENT') {
-                ap.recordRequestPath(reqStream.session, reqPath, false);
-                res.code(404).send();
-              } else {
-                res.code(500).send(err);
-              }
-            })
-        .on('end',
-            () => {
-              ap.recordRequestPath(reqStream.session, reqPath, true);
-            })
-        .pipe(res.res as stream.Writable);
-    ap.push(reqStream);
   });
 
   if (done) done();
