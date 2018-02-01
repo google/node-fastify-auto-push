9013e76b95039fcb6ab2ab245836adc751e1758f
Matteo Collina <hello@matteocollina.com>
2018-02-01 16:23:36 +0100

    Updated fastify. Added fastify-static. Use hooks instead.

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -57,7 +57,7 @@ async function staticServeFn(
     app: fastify.FastifyInstance<HttpServer, RawRequest, RawResponse>,
     opts: AutoPushOptions): Promise<void> {
   const root = opts.root;
-  const prefix = opts.prefix;
+  const prefix = opts.prefix || '';
   const ap = new autoPush.AutoPush(root, opts.cacheConfig);
 
   app.register(require('fastify-static'), opts);
@@ -66,8 +66,9 @@ async function staticServeFn(
     if (isHttp2Request(req)) {
       const reqStream = req.stream;
       const url: string = req.url;
-      const queryIndex: number = url.indexOf('?');
-      const reqPath: string = url.slice(0, queryIndex);
+      let reqPath: string = url.split('?')[0];
+      reqPath = reqPath.replace(prefix, '');
+      console.log('reqPath is', reqPath);
       (req.stream as StorePath).__req_path = reqPath;
       const cookies = cookie.parse(req.headers['cookie'] as string || '');
       const cacheKey = cookies[CACHE_COOKIE_KEY];
@@ -76,14 +77,19 @@ async function staticServeFn(
       // TODO(jinwoo): Consider making this persistent across sessions.
       res.setHeader(
           'set-cookie', cookie.serialize(CACHE_COOKIE_KEY, newCacheKey));
+
+      ap.push(reqStream).then(noop, noop);
     }
   });
 
-  app.addHook('onResponse', async (res) => {
+  app.addHook('onSend', async (request, reply, payload) => {
+    console.log('onSend')
+    const res = reply.res;
     if (isHttp2Response(res)) {
       const resStream = (res as http2.Http2ServerResponse).stream;
-      const statusCode = (res as http.ServerResponse).statusCode;
-      console.log(statusCode, (resStream as StorePath).__req_path)
+      const statusCode = (res as http2.Http2ServerResponse).statusCode;
+      console.log('onSend', statusCode, (resStream as StorePath).__req_path)
+      console.log('hasSession', !!resStream.session)
       if (statusCode === 404) {
         ap.recordRequestPath(
             resStream.session, (resStream as StorePath).__req_path || '',
@@ -95,5 +101,7 @@ async function staticServeFn(
     }
   });
 }
+
+function noop () {}
 
 export const staticServe = fp(staticServeFn);
